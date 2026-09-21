34223e795b260c0dd128e84809c6b2aba8669297
Godson Rapture Chijioke <coolpythoncodes@gmail.com>
2026-09-21 01:10:53 +0100

    chore: initialize NestJS backend

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller.js';
+import { AppService } from './app.service.js';
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
